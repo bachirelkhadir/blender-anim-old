595f359d9a049ad9441f7a46566b5721f4fdca77
bachirelkhadir <bachir009+git@gmail.com>
2021-06-28 00:44:43 -0400

    sample_scenes/hello_world.py

--- sample_scenes/hello_world.py
+++ sample_scenes/hello_world.py
@@ -9,16 +9,18 @@ import mathutils
 
 class HelloWorldScene(Scene):
     def construct(self):
-        lab = self.add_text_facing_camera("f(x) = 0")
+        lab = self.add_text_facing_camera("$f(x) = 0$")
+        cube = self.add_cube()
         self.print_scene_outline()
         self.play(Appear(lab))
+        self.play(Appear(cube))
         self.play(Translate(lab, UP))
         self.wait()
 
     def add_text_facing_camera(self, s):
         lab = self.add_text(s)
         lab.rotation_euler =  bpy.data.objects["Camera"].rotation_euler
-        lab.scale = (2,2,2)
+        lab.scale = (1,1,1)
         lab.location[0] = 3
         utils.bpy_apply_transform(lab, location=False, rotation=False, scale=True)
         color_bpy_object(lab, BABY_YELLOW)
