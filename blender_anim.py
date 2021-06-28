e97748cd596edd45540f10d1b77d0513f710061c
bachirelkhadir <bachir009+git@gmail.com>
2021-06-28 17:58:40 -0400

    blender_anim.py

--- blender_anim.py
+++ blender_anim.py
@@ -8,7 +8,7 @@ import bpy
 import logging
 from src.consts import *
 
-logging.basicConfig(level=logging.ERROR,
+logging.basicConfig(level=logging.INFO,
                     format="%(levelname)-5s: %(name)-9s | %(asctime)-15s | %(message)s")
 log = logging.getLogger(__name__)
 log.info(f"Paths: {EXTRA_PATHS} added to path")
