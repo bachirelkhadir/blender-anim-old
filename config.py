c1e6b9ce2a0068b381aca6c698278f640eae2ce5
bachirelkhadir <bachir009+git@gmail.com>
2021-06-30 12:32:20 -0400

    config.py

--- config.py
+++ config.py
@@ -9,6 +9,8 @@ CONFIG = {
     'BLENDER_BIN': "blender",
     'XDG_OPEN': "open", # "xdg-open"
     'ENGINE': 'BLENDER_EEVEE',
+    'FPS': '60',
+    'Resolution': (1920, 1080),
     #'ENGINE': 'BLENDER_WORKBENCH',
     #'ENGINE': 'CYCLES'
     }
