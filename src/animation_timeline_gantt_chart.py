ce45e762b88116c1618e54a859e7d81293857a3c
bachirelkhadir <bachir009+git@gmail.com>
2021-06-30 19:06:43 -0400

    src/animation_timeline_gantt_chart.py

--- src/animation_timeline_gantt_chart.py
+++ src/animation_timeline_gantt_chart.py
@@ -5,4 +5,8 @@ import pandas as pd
 
 def outline_to_gantt(outline):
     outline = pd.DataFrame(outline)
+    fig = px.timeline(outline, x_start="start", x_end="end", y="name",
+                      color="animation")
+    fig.update_yaxes(autorange="reversed") # otherwise tasks are listed from the bottom up
+    fig.show()
     print(outline.head())
