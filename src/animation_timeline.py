d60559bf709ab2db2feb6c45597bc4c1278b799f
bachirelkhadir <bachir009+git@gmail.com>
2021-06-30 19:07:20 -0400

    src/animation_timeline.py

--- src/animation_timeline.py
+++ src/animation_timeline.py
@@ -56,4 +56,5 @@ class AnimationTimeline:
 
     def print_outline(self):
         from src import animation_timeline_gantt_chart
-        animation_timeline_gantt_chart.outline_to_gantt((self._outline))
+        fig = animation_timeline_gantt_chart.outline_to_gantt(self._outline)
+        fig.save("/tmp/animation_outline.png")
