332b18e5510d6b5aec818e2566f67f9b19ab51e9
bachirelkhadir <bachir009+git@gmail.com>
2021-06-30 19:05:14 -0400

    src/animation_timeline.py

--- src/animation_timeline.py
+++ src/animation_timeline.py
@@ -55,9 +55,5 @@ class AnimationTimeline:
 
 
     def print_outline(self):
-
-        for ob, animation_list in self._outline.items():
-            print(ob, ":", "->".join(map(str, animation_list)))
-
         from src import animation_timeline_gantt_chart
         animation_timeline_gantt_chart.outline_to_gantt((self._outline))
