2d7351d21eb4a7f50b64a51449ebcf59a2ba1d0b
bachirelkhadir <bachir009+git@gmail.com>
2021-06-30 19:22:03 -0400

    src/animation_timeline_gantt_chart.py

--- src/animation_timeline_gantt_chart.py
+++ src/animation_timeline_gantt_chart.py
@@ -29,12 +29,13 @@ px._core.process_dataframe_timeline = my_process_dataframe_timeline
 
 def outline_to_gantt(outline):
     outline = pd.DataFrame(outline)
-    fig = px.timeline(outline, x_start="start", x_end="end", y="name",
-                      color="animation")
-    fig.update_yaxes(autorange="reversed") # otherwise tasks are listed from the bottom up
+
     outline["start"] += 1
     outline["end"] += 10
     outline['delta'] = outline['end'] - outline['start']
+    fig = px.timeline(outline, x_start="start", x_end="end", y="name",
+                      color="animation")
+    fig.update_yaxes(autorange="reversed") # otherwise tasks are listed from the bottom up
     print(outline.head())
     fig.layout.xaxis.type = 'linear'
     #fig.data[0].x = outline.delta.tolist()
