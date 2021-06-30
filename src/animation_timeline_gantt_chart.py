ec79e82b181d28a4cf316b572b1ff6a15a6c8c9e
bachirelkhadir <bachir009+git@gmail.com>
2021-06-30 19:13:45 -0400

    src/animation_timeline_gantt_chart.py

--- src/animation_timeline_gantt_chart.py
+++ src/animation_timeline_gantt_chart.py
@@ -8,4 +8,8 @@ def outline_to_gantt(outline):
     fig = px.timeline(outline, x_start="start", x_end="end", y="name",
                       color="animation")
     fig.update_yaxes(autorange="reversed") # otherwise tasks are listed from the bottom up
+    outline['delta'] = outline['start'] - outline['end']
+    fig.layout.xaxis.type = 'linear'
+    fig.data[0].x = df.delta.tolist()
+    f = fig.full_figure_for_development(warn=False)
     return fig
