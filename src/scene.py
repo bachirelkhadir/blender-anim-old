c34ec2a2b556c4fca9d1f1d05ed00b537d6a5e42
bachirelkhadir <bachir009+git@gmail.com>
2021-07-06 19:59:45 -0400

    src/scene.py

--- src/scene.py
+++ src/scene.py
@@ -53,8 +53,7 @@ class Scene:
         Make plane and add it the `Creation` collection
         """
         plane = basic_geometry.make_plane(loc, scale, name)
-
-        color_bpy_object(cube, BABY_YELLOW)
+        color_bpy_object(plane, BABY_YELLOW)
         return self.add_bpy_object(plane)
 
     def add_line(self, start, end, thickness, name="Line"):
