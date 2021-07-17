0d156657646a7790a1da92e8a1fe8269c084d0ee
bachirelkhadir <bachir009+git@gmail.com>
2021-07-17 15:49:39 -0400

    src/animation_timeline.py

--- src/animation_timeline.py
+++ src/animation_timeline.py
@@ -23,9 +23,6 @@ class AnimationTimeline:
 
     def wait(self, duration):
         self._current_frame += self._duration_to_number_frames(duration)
-        print("-------")
-        print(locals())
-        print(self._current_frame)
         return self._current_frame
 
     def play_animation(self, animation, duration, start_frame=None):
