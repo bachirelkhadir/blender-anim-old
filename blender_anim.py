4f19dd946cbda03eefd054aa793dab1402dccc0d
bachirelkhadir <bachir009+git@gmail.com>
2021-08-02 00:13:30 -0400

    blender_anim.py

--- blender_anim.py
+++ blender_anim.py
@@ -45,8 +45,6 @@ def save_and_render(class_scene, start=None, end=None, **scene_kwargs):
 
 
 
-    # XXXXX
-    return
     if start is None:
         start = 1
     if  end is None:
@@ -60,6 +58,8 @@ def save_and_render(class_scene, start=None, end=None, **scene_kwargs):
     save_blend_file(os.path.join(destination, f"{class_name}.blend"))
     scene.print_animation_outline()
 
+    # XXXXX
+    return
     log.info(f"Start @ frame {start} and end @ {end}")
 
     if cmd_args.render_pngs:
